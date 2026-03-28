09d69648e32170805903e8cbf7cbff6d76e3476a
putriagis <putribalqisnurhayati@gmail.com>
2026-03-28 21:57:33 +0700

    menambahkan class DataGeneric

--- TP_MOD5_103022400081/Program.cs
+++ TP_MOD5_103022400081/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("TP MOD 5");
+﻿using System;
+
+namespace TP_MOD5_103022400081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataGeneric<string> data = new DataGeneric<string>("103022400081");
+            data.PrintData();
+        }
+    }
+}
