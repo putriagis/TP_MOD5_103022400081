f055daa54d46d7e0600622a905af05c33e7dada8
putriagis <putribalqisnurhayati@gmail.com>
2026-03-28 21:26:00 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- TP_MOD5_103022400081/Program.cs
+++ TP_MOD5_103022400081/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("TP MOD 5");
+﻿using System;
+
+namespace TP_MOD5_103022400081
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser<string>("x");
+        }
+    }
+}
